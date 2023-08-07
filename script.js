74800eea488afa44cb8ccc6c380745d6b9d40d39
Monijesu91 <ajitenamonijesu2016@gmail.com>
2023-08-07 11:11:46 +0100

    made webpage responsive, made backgound and color change for only one element at a time

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ function getRandomColor() {
     return color;
 }
 function changeColor(element) {
-    const bodyColor = getRandomColor();
-    const nameColor = getRandomColor();
-
-    document.body.style.backgroundColor = bodyColor;
-    element.style.color = nameColor;
+    const names= document.querySelectorAll (".name");
+    names.forEach(name => {
+        if (name === element) {
+            const color= getRandomColor();
+name.style.backgroundColor = color;
+name.style.color= getConstrastColor(color);
+        } 
+    })
 }
+ function getConstrastColor(hexColor) {
+    const r = parseInt(hexColor.substr(1,2), 16);
+    const g = parseInt(hexColor.substr(3,2), 16);
+    const b = parseInt(hexColor.substr(5,2), 16);
+    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+    return luminance > 0.5 ? "#000000" : "#ffffff";
+ }
